e5aef7fc98b19b0633762268f4ffeea15efeb594
Vinay <vinayd098@gmail.com>
2024-08-07 23:15:26 +0530

    Added Header and Hero Section components

--- taskambitUI/src/App.jsx
+++ taskambitUI/src/App.jsx
@@ -1,11 +1,11 @@
 import './App.css'
-import style from "./"
+import Home from './components/home/Home'
+// import style from "./"
 function App() {
   return(
-   <div>
-    <h1 className="text-3xl font-bold underline">Hello React JS</h1>
-    <h1 className="">CHETHANA B</h1>
-   </div>
+   <>
+    <Home />
+   </>
   )
 }
 export default App
---
+++ taskambitUI/src/components/home/Home.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+import HeroSection from './HeroSection'
+import WorkWithUs from './WorkWithUs'
+import Header from './Header'
+
+const Home = () => {
+  return (
+    <>
+        <Header />
+        {/* <HeroSection /> */}
+        <WorkWithUs />
+    </>
+  )
+}
+
+export default Home
